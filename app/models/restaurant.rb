728b33dc57d799d78cedb94b9a098c66d80c279a
Adam Cohn <adamcohn88@gmail.com>
2018-05-07 10:32:22 -0400

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,26 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def reviews
+    Review.all.select { |r| r.restaurant == self }
+  end
+
+  def customers
+    Review.all.map { |r| r.customer if r.restaurant == self }.compact
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |r| r.name == name }
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,17 @@ def reload
   load 'config/environment.rb'
 end
 
+bob = Customer.new('Bob', 'Smith')
+joe = Customer.new('Joe', 'Johnson')
+sally = Customer.new('Sally', 'Smith')
+adam = Customer.new('Adam', 'Cohn')
+joes = Customer.new('Joe', 'Sullivan')
+chipotle = Restaurant.new('Chipotle')
+rice_bar = Restaurant.new('Rice Bar')
+pret = Restaurant.new('Pret')
+bob.add_review(chipotle, 'One star: got E-Coli')
+adam.add_review(chipotle, "gross.")
+adam.add_review(rice_bar, "10/5; I wouldn't eat anywhere else.")
+sally.add_review(pret, "Soggy sandwiches. Needless to say, I won't be returning")
+
 Pry.start
